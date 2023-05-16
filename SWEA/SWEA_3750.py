d0dbc3c644d0f88e445e15ccd12bc694b3d1ebf3
HeesuNam <skagmltn7@hanmail.net>
2023-05-16 22:33:46 +0900

    problem 3750

---
+++ SWEA/SWEA_3750.py
@@ -0,0 +1,19 @@
+"""
+1. 아이디어
+output 한자리 수 가 될때까지의 f(n)
+    while len(n)!=1
+    - f(n)구현
+        -각 자리수 계산
+        -n 다시 만들기
+        
+"""
+T = int(input())
+li=[]
+for tc in range(1,T+1):
+    li.append(input())
+
+for i, num in enumerate(li):
+    while len(num)!=1:
+        num = sum(map(int,num))
+        num = str(num)
+    print('#{} {}'.format(i+1,num))
