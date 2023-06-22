5cbad1a21ed8066d646f0e7036325cfc4b3a4e7c
HeesuNam <skagmltn7@hanmail.net>
2023-06-22 18:15:12 +0900

    add edge case

--- BOJ/BOJ_11404.py
+++ BOJ/BOJ_11404.py
@@ -7,17 +7,19 @@ import sys; input = sys.stdin.readline
 n = int(input())
 m = int(input())
 
-costs = [[float('inf') for _ in range(n+1)] for _ in range(n+1)]
+costs = [[0 if i==j else float('inf') for j in range(n+1)] for i in range(n+1)]
+
 for _ in range(m):
     a, b, cost = map(int,input().split())
     costs[a][b]=min(costs[a][b],cost)
 
 for k in range(1,n+1):
     for i in range(1,n+1):
-        if i==k: costs[i][k] = 0
-        else: costs[i][k] = costs[i][k]
         for j in range(1,n+1):
             costs[i][j] = min(costs[i][j], costs[i][k]+costs[k][j])
 
 for i in range(1,n+1):
-    print(' '.join(map(str,costs[i][1:])))
+    for j in range(1,n+1):
+        if costs[i][j] == float('inf'): costs[i][j] = 0
+        print(costs[i][j],end=' ')
+    print()
