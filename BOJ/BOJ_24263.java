f8ee54c8a644435d6071587c2d45ab2cccc11e08
Heesu <133394749+skagmltn7@users.noreply.github.com>
2023-07-11 08:41:32 +0900

    Create BOJ_24263.java

---
+++ BOJ/BOJ_24263.java
@@ -0,0 +1,10 @@
+package BOJ;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(br.readLine());
+		System.out.println(1);
+	}
+}
