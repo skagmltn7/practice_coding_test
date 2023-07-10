58771e1e9ca44bc894858f3778124bbfb4a87056
Heesu <133394749+skagmltn7@users.noreply.github.com>
2023-07-11 08:43:40 +0900

    Create BOJ_4470.java

---
+++ BOJ/BOJ_4470.java
@@ -0,0 +1,12 @@
+package BOJ;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.valueOf(br.readLine());
+		for(int i= 0; i<n; i++) {
+			System.out.printf("%d. %s\n", i+1,br.readLine());
+		}
+	}
+}
