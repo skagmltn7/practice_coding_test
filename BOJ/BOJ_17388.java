aedc639c13bda062903d7ca80c5f31665cf02c99
HeesuNam <skagmltn7@hanmail.net>
2023-07-15 13:04:21 +0900

    BOJ 5639

--- BOJ/BOJ_17388.java
+++ BOJ/BOJ_17388.java
@@ -2,7 +2,7 @@ package BOJ;
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class BOJ_17388 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
---
+++ BOJ/BOJ_5639.py
@@ -0,0 +1,39 @@
+"""
+1. 아이디어
+- 전위 순회
+left 서브트리
+    -  before보다 작을경우
+        before를 부모등록 
+        left등록
+    - 클 경우
+        before보다 크고 before의 부모보다 작으면 before의 right등록
+        before의 부모보다 크면 before = before.parent
+2. 시간복잡도
+n = 1e4
+"""
+import sys
+
+input = sys.stdin.readline
+
+
+def post(start, end):
+    if start > end:
+        return
+    mid = end + 1
+    for i in range(start + 1, end + 1):
+        if nums[i] > nums[start]:
+            mid = i
+            break
+    post(start + 1, mid - 1)
+    post(mid, end)
+    print(nums[start])
+
+
+nums = []
+while True:
+    try:
+        nums.append(int(input()))
+    except:
+        break
+
+post(0, len(nums) - 1)
