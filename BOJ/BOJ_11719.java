4dd61578e93fdf069e4ee800c2282bbec5c0ab11
Heesu <133394749+skagmltn7@users.noreply.github.com>
2023-07-24 12:52:51 +0900

    Create BOJ_11719.java

---
+++ BOJ/BOJ_11719.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class BOJ_11719 {
+	public static void main (String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = "";
+		while((input = br.readLine()) != null) {
+			System.out.println(input);
+		}
+	}
+}
