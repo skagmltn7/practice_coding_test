d832ada0404bd8a661fa9278ff2e25d5ca4f8557
Heesu <133394749+skagmltn7@users.noreply.github.com>
2024-03-23 01:35:16 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 128 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int B = Integer.parseInt(br.readLine());
+    System.out.println(B/11*10);
+  } // end of class
+}// end of main
