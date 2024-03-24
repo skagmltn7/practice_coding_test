343ee1bc8b65c6fe66da796238cccc98e6f0051c
Heesu <133394749+skagmltn7@users.noreply.github.com>
2024-03-25 00:18:03 +0900

    [Bronze V] Title: 果物 (Fruit), Time: 128 ms, Memory: 14052 KB -BaekjoonHub

---
+++ 백준/Bronze/31606. 果物 （Fruit）/果物 （Fruit）.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int X = Integer.parseInt(br.readLine());
+    int Y = Integer.parseInt(br.readLine());
+    System.out.println(X + Y + 3);
+  } // end of class
+}// end of main
